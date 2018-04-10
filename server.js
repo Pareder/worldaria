744585b2c267aebbee85b6e803aa54aada9f517d
Anton Chernyavsky <abc@abc.com>
2018-04-10 20:09:14 +0300

    change server config

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-var compression = require('compression');
-app = express();
-app.use(compression());
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+const express = require('express')
+const path = require('path')
+const history = require('connect-history-api-fallback')
+const compression = require('compression')
+
+const app = express()
+const staticFileMiddleware = express.static(path.join(__dirname + "/dist"))
+
+app.use(compression())
+app.use(staticFileMiddleware)
+app.use(history())
+app.use(staticFileMiddleware)
+
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/index.html'))
+})
+
+app.listen(5000, function () {
+  console.log('Express is serving on 5000!')
+})
