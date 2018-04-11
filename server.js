f4f7060e65b17db4e3bf8c479a71235acb3bc023
Anton Chernyavsky <abc@abc.com>
2018-04-11 21:06:54 +0300

    change server settings

--- server.js
+++ server.js
@@ -4,7 +4,17 @@ const history = require('connect-history-api-fallback')
 const compression = require('compression')
 
 const app = express()
-const staticFileMiddleware = express.static(path.join(__dirname + "/dist"))
+const staticFileMiddleware = express.static(path.join(__dirname + "/dist"), {
+  maxAge: 604800000,
+  setHeaders: function (res, path) {
+    res.setHeader('X-FRAME-OPTIONS', 'DENY')
+    res.setHeader('X-XSS-Protection', '1; mode=block')
+    res.setHeader('X-Content-Type-Options', 'nosniff')
+    res.setHeader("Content-Security-Policy", "script-src 'self' https://apis.google.com 'unsafe-inline' 'unsafe-eval'")
+    res.setHeader('Cache-Control', 'max-age=604800000')
+    res.setHeader('Expires', new Date(Date.now() + 2592000000 * 30).toUTCString())
+  }
+})
 
 app.use(compression())
 app.use(staticFileMiddleware)
