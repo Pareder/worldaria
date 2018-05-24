538c8cf59d2a02e1353e200e3c7013d03d56bab4
Anton Chernyavsky <abc@abc.com>
2018-05-24 21:16:26 +0300

    changed server settings

--- server.js
+++ server.js
@@ -29,13 +29,14 @@ app.get('/', (req, res) => {
 
 app.use(express.json())
 
-const server = app.listen(5000, () => {
+const server = http.createServer(app).listen(process.env.PORT || 5000, () => {
   console.log("Express server listening on port 5000");
 })
 
 const io = socket.listen(server)
 
 const USERS = {}
+const ONLINE_USERS = {}
 let ROOM_NUMBER = 0
 const COUNTRIES = []
 
@@ -51,30 +52,72 @@ io.on('connection', (socket) => {
 
   socket.on('sendName', (data) => {
     socket.username = data
+    ONLINE_USERS[data] = socket.id
+    io.emit('getOnlineUsers', Object.keys(ONLINE_USERS))
+  })
+  socket.on('enterOnlineMode', () => {
+    socket.emit('getOnlineUsers', Object.keys(ONLINE_USERS))
+  })
+  socket.on('sendInvite', (data) => {
+    io.to(ONLINE_USERS[data.opponentName]).emit('getInvite', data)
+  })
+  socket.on('cancelInvite', (data) => {
+    io.to(ONLINE_USERS[data.opponentName]).emit('declineInvite')
+  })
+  socket.on('makeDecision', (data) => {
+    if (data.status) {
+      if (socket.room) {
+        delete USERS[socket.room]
+        socket.room = null
+      }
+      socket.room = `room${ROOM_NUMBER}`
+      USERS[socket.room] ? USERS[socket.room] = USERS[socket.room] : USERS[socket.room] = []
+      USERS[socket.room].push(data.myName)
+      socket.join(socket.room)
+      delete ONLINE_USERS[socket.username]
+      io.emit('getOnlineUsers', Object.keys(ONLINE_USERS))
+    }
+    io.to(ONLINE_USERS[data.opponentName]).emit('opponentsDecision', data.status)    
+  })
+  socket.on('createGame', (data) => {
     socket.room = `room${ROOM_NUMBER}`
     USERS[socket.room] ? USERS[socket.room] = USERS[socket.room] : USERS[socket.room] = []
-    USERS[socket.room].push(data)
+    USERS[socket.room].unshift(data)
     socket.join(socket.room)
     if (USERS[socket.room].length === 2) {
-      io.sockets.in(socket.room).emit('startGame', { users: USERS[socket.room], countries: [...COUNTRIES.sort(compareRandom)]})
+      io.sockets.in(socket.room).emit('startGame', { users: USERS[socket.room], countries: [...COUNTRIES.sort(compareRandom)] })
+      delete ONLINE_USERS[socket.username]
+      io.emit('getOnlineUsers', Object.keys(ONLINE_USERS))
       ROOM_NUMBER++
     }
   })
   socket.on('countryClick', (data) => {
-    io.sockets.in(socket.room).emit('checkAnswer', data)
+    socket.broadcast.to(socket.room).emit('checkAnswer', data)
   })
   socket.on('userLeft', () => {
-    io.sockets.in(socket.room).emit('endMatch')
+    io.sockets.in(socket.room).emit('endMatch')    
     delete USERS[socket.room]
     socket.leave(socket.room)
+    ONLINE_USERS[socket.username] = socket.id
+    io.emit('getOnlineUsers', Object.keys(ONLINE_USERS))
   })
-  socket.on('gameResult', () => {
-    delete USERS[socket.room]
-    socket.leave(socket.room)
+  socket.on('revenge', (data) => {
+    socket.broadcast.to(socket.room).emit('opponentsRevenge')
+  })
+  socket.on('revengeDecision', () =>{
+    io.sockets.in(socket.room).emit('revengeGame', [...COUNTRIES.sort(compareRandom)])
   })
   socket.on('disconnect', () => {
     io.sockets.in(socket.room).emit('endMatch')
     delete USERS[socket.room]
+    if (ONLINE_USERS[socket.username]) {
+      delete ONLINE_USERS[socket.username]
+      io.emit('getOnlineUsers', Object.keys(ONLINE_USERS))
+    }
     socket.leave(socket.room)
+  })
+  socket.on('signOut', () => {
+    delete ONLINE_USERS[socket.username]
+    io.emit('getOnlineUsers', Object.keys(ONLINE_USERS))
   })
 })
