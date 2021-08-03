55e31cca01d595b2caa15d0d88137a0dc1003e3f
Pareder <ruskc2beat@gmail.com>
2021-08-03 16:19:46 +0300

    Add support for Area mode on mobile devices

--- src/utils/Drag.js
+++ src/utils/Drag.js
@@ -8,6 +8,9 @@ class Drag {
     this.dragClickOffsetY = null
     this.lastDragX = null
     this.lastDragY = null
+    this.onDragStart = this.onDragStart.bind(this)
+    this.onDragOver = this.onDragOver.bind(this)
+    this.onDragEnd = this.onDragEnd.bind(this)
     this.init()
   }
 
@@ -15,41 +18,61 @@ class Drag {
     this.node.draggable = true
     this.node.style.userSelect = 'none'
 
-    this.node.addEventListener('dragstart', event => {
-      this.dragClickOffsetX = event.layerX
-      this.dragClickOffsetY = event.layerY
-      this.makeClone()
-
-      event.target.style.opacity = 0
-    })
-
+    this.node.addEventListener('dragstart', this.onDragStart)
+    this.node.addEventListener('touchstart', this.onDragStart)
     document.addEventListener('dragenter', event => event.preventDefault())
+  }
 
-    document.addEventListener('dragover', event => {
-      event.preventDefault()
+  onDragStart(e) {
+    const event = this.getEvent(e)
+    console.log(event)
+    this.dragClickOffsetX = event.layerX || event.clientX
+    this.dragClickOffsetY = event.layerY || event.clientY
+    this.makeClone()
 
-      let { x, y } = event
-      // Odd glitch
-      if (x === 0 && y === 0) {
-        x = this.lastDragX
-        y = this.lastDragY
-      }
+    this.node.style.opacity = 0
 
-      if (x === this.lastDragX && y === this.lastDragY) {
-        return
-      }
+    document.addEventListener('dragover', this.onDragOver)
+    document.addEventListener('touchmove', this.onDragOver)
 
-      this.translate(x - this.dragClickOffsetX, y - this.dragClickOffsetY)
-      this.lastDragX = x
-      this.lastDragY = y
-    })
+    document.addEventListener('drop', this.onDragEnd)
+    document.addEventListener('touchend', this.onDragEnd)
+  }
 
-    document.addEventListener('drop', event => {
-      this.node.style.opacity = 1
-      this.clone.parentNode.removeChild(this.clone)
-      this.clone = null
-      this.onDrop(event)
-    })
+  onDragOver(e) {
+    e.preventDefault()
+    const event = this.getEvent(e)
+
+    let { clientX, clientY } = event
+    // Odd glitch
+    if (clientX === 0 && clientY === 0) {
+      clientX = this.lastDragX
+      clientY = this.lastDragY
+    }
+
+    if (clientX === this.lastDragX && clientY === this.lastDragY) {
+      return
+    }
+
+    console.log(clientX, this.dragClickOffsetX)
+    console.log(clientY, this.dragClickOffsetY)
+    this.translate(clientX - this.dragClickOffsetX, clientY - this.dragClickOffsetY)
+    this.lastDragX = clientX
+    this.lastDragY = clientY
+  }
+
+  onDragEnd(e) {
+    const event = this.getEvent(e)
+    this.node.style.opacity = 1
+    // this.clone.parentNode.removeChild(this.clone)
+    // this.clone = null
+    this.onDrop(event)
+
+    document.removeEventListener('dragover', this.onDragOver)
+    document.removeEventListener('touchmove', this.onDragOver)
+
+    document.removeEventListener('drop', this.onDragEnd)
+    document.removeEventListener('touchend', this.onDragEnd)
   }
 
   makeClone() {
@@ -71,6 +94,15 @@ class Drag {
   translate(x, y) {
     this.clone.style.left = x + 'px'
     this.clone.style.top = y + 'px'
+  }
+
+  getEvent(event) {
+    // To handle touch events because their objects differ from original drag event objects
+    return event.targetTouches && event.targetTouches.length
+      ? event.targetTouches[0]
+      : event.changedTouches && event.changedTouches.length
+        ? event.changedTouches[0]
+        : event
   }
 }
 
